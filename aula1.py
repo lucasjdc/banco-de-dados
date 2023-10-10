f2be9723842131deb8629929344af1e0cc150523
Lucas Costa <106778205+lucasjdc@users.noreply.github.com>
2023-10-09 21:49:20 -0300

    links de dados em código

---
+++ aula1.py
@@ -0,0 +1,4 @@
+url_itens_pedidos = 'https://github.com/alura-cursos/SQL-python-integracao/raw/main/TABELAS/itens_pedidos.csv'
+url_pedidos = 'https://github.com/alura-cursos/SQL-python-integracao/raw/main/TABELAS/pedidos.csv'
+url_produto = 'https://github.com/alura-cursos/SQL-python-integracao/raw/main/TABELAS/produtos.csv'
+url_vendedores = 'https://github.com/alura-cursos/SQL-python-integracao/raw/main/TABELAS/vendedores.csv'
