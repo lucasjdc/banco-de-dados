0f09b7efad02e79a89c0a73065fe1f44767922eb
lucasjdc <lucasjpdc@gmail.com>
2023-10-09 22:38:56 -0300

    Selecionando dados

---
+++ aula2.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sqlalchemy import create_engine, inspect, text
+
+url_itens_pedidos = 'https://github.com/alura-cursos/SQL-python-integracao/raw/main/TABELAS/itens_pedidos.csv'
+url_pedidos = 'https://github.com/alura-cursos/SQL-python-integracao/raw/main/TABELAS/pedidos.csv'
+url_produto = 'https://github.com/alura-cursos/SQL-python-integracao/raw/main/TABELAS/produtos.csv'
+url_vendedores = 'https://github.com/alura-cursos/SQL-python-integracao/raw/main/TABELAS/vendedores.csv'
+
+itens_pedidos = pd.read_csv(url_itens_pedidos)
+pedidos = pd.read_csv(url_pedidos)
+produtos = pd.read_csv(url_produto)
+vendedores = pd.read_csv(url_vendedores)
+
+engine = create_engine('sqlite:///:memory:')
+
+produtos.to_sql('produtos', engine, index=False)
+itens_pedidos.to_sql('itens_pedidos', engine, index=False)
+pedidos.to_sql('pedidos', engine, index=False)
+vendedores.to_sql('vendedores', engine, index=False)
+
+inspector = inspect(engine)
+print(inspector.get_table_names())
+
+query = 'SELECT CONDICAO FROM PRODUTOS'
+
+with engine.connect() as  conexao:
+    consulta = conexao.execute(text(query))
+    dados = consulta.fetchall()
+
+pd.DataFrame(dados,columns=consulta.keys())
+
+def sql_def(query):
+    with engine.connect() as  conexao:
+        consulta = conexao.execute(text(query))
+        dados = consulta.fetchall()
+
+    return pd.DataFrame(dados,columns=consulta.keys())
