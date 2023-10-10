71b40bbeffe1bdc9eb9b3e32b137d970bd2dcb76
lucasjdc <lucasjpdc@gmail.com>
2023-10-09 22:00:36 -0300

    Conhecendo as ferramentas

--- aula1.py
+++ aula1.py
@@ -1,3 +1,7 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sqlalchemy import create_engine, inspect, text
+
 url_itens_pedidos = 'https://github.com/alura-cursos/SQL-python-integracao/raw/main/TABELAS/itens_pedidos.csv'
 url_pedidos = 'https://github.com/alura-cursos/SQL-python-integracao/raw/main/TABELAS/pedidos.csv'
 url_produto = 'https://github.com/alura-cursos/SQL-python-integracao/raw/main/TABELAS/produtos.csv'
