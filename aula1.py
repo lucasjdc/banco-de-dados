d358414aa6400a6f7a3b0f6abf549a6cf7fb6cf7
lucasjdc <lucasjpdc@gmail.com>
2023-10-09 22:10:49 -0300

    Coletando os dados

--- aula1.py
+++ aula1.py
@@ -6,3 +6,8 @@ url_itens_pedidos = 'https://github.com/alura-cursos/SQL-python-integracao/raw/m
 url_pedidos = 'https://github.com/alura-cursos/SQL-python-integracao/raw/main/TABELAS/pedidos.csv'
 url_produto = 'https://github.com/alura-cursos/SQL-python-integracao/raw/main/TABELAS/produtos.csv'
 url_vendedores = 'https://github.com/alura-cursos/SQL-python-integracao/raw/main/TABELAS/vendedores.csv'
+
+itens_pedidos = pd.read_csv(url_itens_pedidos)
+pedidos = pd.read_csv(url_pedidos)
+produtos = pd.read_csv(url_produto)
+vendedores = pd.read_csv(url_vendedores)
