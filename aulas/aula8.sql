45b912849b6aa6cb73a7c9bba41c59179918d767
lucasjdc <lucasjpdc@gmail.com>
2023-10-19 21:51:59 -0300

    Criando um banco de dados

---
+++ aulas/aula8.sql
@@ -0,0 +1,22 @@
+-- DML - INSERT
+
+-- insert into <tabela> (colunas) values (valores)
+
+-- insert  into tipocliente values (01,'Pessoa Física');
+-- insert  into tipocliente values (02,'Pessoa Jurídica');
+-- insert  into tipocliente values (01,'Pessoa Especial');
+
+-- insert into cliente values (01,'Bruno','00000000000','1978-03-07',01)
+insert into cliente values (02,'Lívia','00000000001','1979-03-07',01)
+insert into cliente values (03,'Lucas','00000000002','1980-03-07',01)
+insert into cliente values (04,'Luana','00000000003','1981-03-07',01)
+insert into cliente values (05,'Dirceu','00000000004','1982-03-07',01)
+insert into cliente values (06,'Zuelma','00000000005','1983-03-07',01)
+insert into cliente values (07,'Rita','00000000006','1984-03-07',01)
+insert into cliente values (08,'Mariana','00000000007','1985-03-07',01)
+insert into cliente values (09,'José','00000000008','1986-03-07',01)
+insert into cliente values (10,'Maria','00000000009','1987-03-07',01)
+
+-- DQL - SELECT
+
+SELECT * FROM tipocliente;
